b2d4dbc20cd6d85bf19ef9b94bc652c36befabb9
johnwhiteca <166945848+johnwhiteca@users.noreply.github.com>
2026-07-21 18:09:02 -0400

    Replace initial site with johnwhite.ca Astro foundation

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,6 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import icon from 'astro-icon';
-
-// https://astro.build/config
 export default defineConfig({
-  integrations: [icon()]
+	site: 'https://johnwhite.ca',
 });
